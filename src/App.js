c9ee35baa11dd6c7b7eb951c3440e6b5eb5adcdf
a.lvov@retailrobotics.onmicrosoft.com <a.lvov@retailrobotics.onmicrosoft.com>
2022-10-10 23:27:29 +0300

    first commin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 function App() {
   return (
@@ -17,9 +19,69 @@ function App() {
         >
           Learn React
         </a>
+
+        <CardList apiUrl="http://localhost:5000/api/card" />
+
       </header>
     </div>
   );
 }
+
+class Card extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = { data: props.card };
+  }
+
+  render() {
+      return <div>
+          <p><b>{this.state.data.name}</b></p>
+          <a href={this.state.data.uri}>Link to image </a> 
+      </div>;
+  }
+}
+
+
+class CardList extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = { cards: [] };
+  }
+  // загрузка данных
+  loadData() {
+
+
+      var xhr = new XMLHttpRequest();
+      xhr.open("get", this.props.apiUrl, true);
+      xhr.onload = function () {
+          var data = JSON.parse(xhr.responseText);
+          this.setState({ cards: data });
+      }.bind(this);
+      xhr.send();
+  }
+  componentDidMount() {
+      this.loadData();
+  }
+
+  render() {
+      return <div>
+          <h2>Список смартфонов</h2>
+          <div>
+              {
+                  this.state.cards.map(function (card) {
+
+                      return <Card key={card.id} card={card}  />
+                  })
+              }
+          </div>
+      </div>;
+  }
+}
+
+
+
+
 
 export default App;
